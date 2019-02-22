62a5467ac11efe3b48fce11341a6b42c2b89c6f5
Jake Muller <jemuller@mtu.edu>
2019-02-22 13:35:01 -0500

    Improvments made

--- Week 6/WebBrowser/src/WebBrowser.java
+++ Week 6/WebBrowser/src/WebBrowser.java
@@ -1,6 +1,7 @@
 // IMPORTS
 // These are some classes that may be useful for completing the project.
 // You may have to add others.
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,35 +9,20 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
 import javafx.scene.web.WebHistory;
-import javafx.scene.web.WebHistory.Entry;
 import javafx.stage.Stage;
 import javafx.concurrent.Worker.State;
-import javafx.concurrent.Worker;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 /**
@@ -50,8 +36,8 @@ public class WebBrowser extends Application {
     private BorderPane borderPane = null;
     private WebView view = null;
     private WebEngine webEngine = null;
-    private WebHistory webHistory =null;
-    private TextField statusbar = null;
+    private WebHistory webHistory = null;
+    private Text statusbar = null;
     private TextField URLfield = null;
     private Button help = null;
     private Button search = null;
@@ -60,13 +46,14 @@ public class WebBrowser extends Application {
 
 
     // HELPER METHODS
+
     /**
      * Retrieves the value of a command line argument specified by the index.
      *
      * @param index - position of the argument in the args list.
      * @return The value of the command line argument.
      */
-    private String getParameter( int index ) {
+    private String getParameter(int index) {
         Parameters params = getParameters();
         List<String> parameters = params.getRaw();
         return !parameters.isEmpty() ? parameters.get(index) : "";
@@ -79,7 +66,7 @@ public class WebBrowser extends Application {
      *
      * @return browser - a WebView container for the WebEngine.
      */
-    private WebView makeHtmlView( ) {
+    private WebView makeHtmlView() {
         view = new WebView();
         webEngine = view.getEngine();
         return view;
@@ -90,40 +77,56 @@ public class WebBrowser extends Application {
      *
      * @return statusbarPane - the HBox layout that contains the statusbar.
      */
-    private HBox makeStatusBar( ) {
+    private HBox makeStatusBar() {
         HBox statusbarPane = new HBox();
         statusbarPane.setPadding(new Insets(5, 4, 5, 4));
         statusbarPane.setSpacing(10);
-        statusbarPane.setStyle("-fx-background-color: #336699;");
-        statusbar = new TextField();
+//        statusbarPane.setStyle("-fx-background-color: #336699;");
+        statusbar = new Text();
         HBox.setHgrow(statusbar, Priority.ALWAYS);
         statusbarPane.getChildren().addAll(statusbar);
         return statusbarPane;
     }
 
     //make toolbar
-    private HBox makeToolBar(){
+    private HBox makeToolBar() {
         webHistory = webEngine.getHistory();
         HBox toolbar = new HBox(5);
         toolbar.setAlignment(Pos.CENTER);
         Label URLLabel = new Label("URL");
         back = new Button("<--");
-        back.setOnAction( e-> {
-            if( webHistory.getCurrentIndex() != 0) {
+        back.setOnAction(e -> {
+            if (webHistory.getCurrentIndex() != 0) {
                 int his = webHistory.getCurrentIndex();
                 webHistory.go(-1);
             }
         });
         //forward button
         forward = new Button("-->");
-        forward.setOnAction( e -> {
+        forward.setOnAction(e -> {
+//          webEngine.executeScript("history.back()");
             int his = webHistory.getCurrentIndex();
             webHistory.go(+1);
         });
         //Search function
         URLfield = new TextField();
+        toolbar.setHgrow(URLfield, Priority.ALWAYS);
         URLfield.setOnAction(e -> {
-            webEngine.load(URLfield.getText());
+            if (URLfield.getText().contains("http://")) {
+                webEngine.load(URLfield.getText());
+            } else if (URLfield.getText().contains("www.") | URLfield.getText().contains("WWW.")) {
+                webEngine.load("http://" + URLfield.getText());
+            } else {
+                String[] words = URLfield.getText().split(" ");
+                String word = "";
+                for (int i = 0; i < words.length; i++) {
+                    word = word + "+" + words[i];
+                }
+                String cutDown = word.substring(1, word.length());
+//                System.out.println(cutDown);
+                webEngine.load("https://www.google.com/search?q=" + cutDown);
+            }
+
         });
         search = new Button("Search");
         search.setOnAction(e -> {
@@ -131,29 +134,42 @@ public class WebBrowser extends Application {
         });
         //help button
         help = new Button("?");
-        help.setOnAction( e-> {
+        help.setOnAction(e -> {
+            webEngine.loadContent("" +
+                    "<HTML>" +
+                        "<HEAD>" +
+                            "<TITLE>Help!</TITLE>" +
+                        "</HEAD>" +
+                        "<H1>This is a help page!</H1>" +
+                        "<BODY>please type a correctly formated URL into the search bar (Example: http://www.mtu.edu )</BODY>");
         });
-        toolbar.getChildren().addAll(back,forward,URLLabel,URLfield,search,help);
+
+        toolbar.getChildren().addAll(back, forward, URLLabel, URLfield, search, help);
         return toolbar;
     }
 
     // REQUIRED METHODS
+
     /**
      * The main entry point for all JavaFX applications. The start method is
      * called after the init method has returned, and after the system is ready
      * for the application to begin running.
-     *
+     * <p>
      * NOTE: This method is called on the JavaFX Application Thread.
      *
      * @param primaryStage - the primary stage for this application, onto which
-     * the application scene can be set.
+     *                     the application scene can be set.
      */
     @Override
     public void start(Stage primaryStage) {
         // Build your window here.
         borderPane = new BorderPane();
         view = makeHtmlView();
+        webEngine.setOnStatusChanged(event -> {
+            statusbar.setText(event.getData());
+        });
         HBox statusHBox = makeStatusBar();
+
         borderPane.setCenter(view);
         borderPane.setBottom(statusHBox);
         borderPane.setTop(makeToolBar());
@@ -161,7 +177,17 @@ public class WebBrowser extends Application {
         webEngine.load("http://www.google.com");
         statusbar.setText("This is the statusBar");
 
-        Scene scene = new Scene(borderPane,800,600);
+        webEngine.getLoadWorker().stateProperty().addListener(
+                new ChangeListener<State>() {
+                    public void changed(ObservableValue ov, State oldState, State newState) {
+                        if (newState == State.SUCCEEDED) {
+                            URLfield.setText(webEngine.getLocation());
+                            primaryStage.setTitle("CS1122 Web Browser: " + webEngine.getLocation());
+                        }
+                    }
+                });
+
+        Scene scene = new Scene(borderPane, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.setTitle("CS1122 Web Browser");
         primaryStage.show();
