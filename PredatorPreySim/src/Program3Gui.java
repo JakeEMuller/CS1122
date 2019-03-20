c8a7c11218338158594389e704cd6e6f21f1466a
JakeEMuller <jemuller@mtu.edu>
2019-03-20 00:57:51 -0400

    Initial Commit

--- PredatorPreySim/src/Program3Gui.java
+++ PredatorPreySim/src/Program3Gui.java
@@ -1,5 +1,7 @@
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventType;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.XYChart;
@@ -17,7 +19,7 @@ import javafx.concurrent.Task;
 public class Program3Gui extends Application {
     static int i = 0;
 
-
+    //runs a tick in a Thread so i does not interfree with data
     public int[] runTicks() {
         int[] data = new int[3];
         Task<Integer> test = new Task<Integer>() {
@@ -41,6 +43,7 @@ public class Program3Gui extends Application {
         return data;
     }
 
+    //used to add Data points to the Gui
     public void updateGui(Series series, int pop, int tick) {
         Data<Number, Number> temp = new Data<>(tick, pop);
         series.getData().add(temp);
@@ -77,16 +80,7 @@ public class Program3Gui extends Application {
         graphMoose.getData().add(moose);
         graphWolf.getData().add(wolf);
 
-        //waste time button
-        Button waste = new Button("Dont waste my motha fuckin time");
-        waste.setOnAction(event -> {
-            try {
-                Thread.sleep(1);
-            } catch (Exception e) {
-                System.out.println("Big woops");
-            }
-
-        });
+        //run
 
         //create 100 tick button
         Button tick100 = new Button("100 ticks");
@@ -124,32 +118,24 @@ public class Program3Gui extends Application {
             }
         });
 
-        // create start button
+        // create start button uses orginial setup for updating graph (Does not live update)
         Button start = new Button("Start Simulation");
 
         start.setOnAction(event -> {
-            while (Test.simulator.getCurrentWolfs().size() != 0 && Test.simulator.getCurrentMoose().size() != 0){
-                tick100.fire();
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    System.out.println("Thread sleep error");
+            while (Test.simulator.getCurrentWolfs().size() != 0 && Test.simulator.getCurrentMoose().size() != 0) {
+                Test.simulator.tick();
+                System.out.println("Tick Number: " + i);
+                System.out.println("There are " + Test.simulator.getCurrentMoose().size() + " Moose");
+                System.out.println("There are " + Test.simulator.getCurrentWolfs().size() + " Wolves\n");
+                if (i % 100 == 0) {
+                    updateGui(moose, Test.simulator.getCurrentMoose().size(), i);
+                    updateGui(wolf, Test.simulator.getCurrentWolfs().size(), i);
                 }
+                i++;
             }
-//            while (Test.simulator.getCurrentWolfs().size() != 0 && Test.simulator.getCurrentMoose().size() != 0) {
-//                Test.simulator.tick();
-//                System.out.println("Tick Number: " + i);
-//                System.out.println("There are " + Test.simulator.getCurrentMoose().size() + " Moose");
-//                System.out.println("There are " + Test.simulator.getCurrentWolfs().size() + " Wolves\n");
-//                if (i % 100 == 0) {
-//                    updateGui(moose, Test.simulator.getCurrentMoose().size(), i);
-//                    updateGui(wolf, Test.simulator.getCurrentWolfs().size(), i);
-//                }
-//                i++;
-//            }
         });
 
-        // real start button
+        // uses new runTicks method, in hopes that thread would allow graph to update. (doesnt live update)
         Button startButton = new Button("start time");
         startButton.setOnAction(event -> {
             while (Test.simulator.getCurrentWolfs().size() != 0 && Test.simulator.getCurrentMoose().size() != 0) {
@@ -166,7 +152,9 @@ public class Program3Gui extends Application {
             }
         });
 
+
         hBox.setSpacing(5);
+        hBox.setAlignment(Pos.CENTER);
         hBox.getChildren().addAll(start,startButton, tick100, tickThousand);
 
         borderPane.setBottom(hBox);
