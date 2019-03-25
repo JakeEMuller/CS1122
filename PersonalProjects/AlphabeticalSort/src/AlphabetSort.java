7f2e16fa1ca8f3918b2fa5ed37fa5c099647d043
JakeEMuller <jemuller@mtu.edu>
2019-03-24 21:56:40 -0400

    Initial Commmit

--- PersonalProjects/AlphabeticalSort/src/AlphabetSort.java
+++ PersonalProjects/AlphabeticalSort/src/AlphabetSort.java
@@ -2,49 +2,51 @@ import java.util.Arrays;
 
 public class AlphabetSort {
 
-    public int[] selectionSort(int[] array) {
-        int n = array.length;
+    public int[] selectionSort(int[] arr) {
+
+        int n = arr.length;
+
 
         for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
             int min_idx = i;
-            for(int j = i+1; j < n; j++ ){
-                if (array[j] < array[min_idx]){
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
                     min_idx = j;
                 }
-
-                int temp = array[min_idx];
-                array[min_idx] = array[i];
-                array[i] = temp;
-
             }
-        }
 
-        return array;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("selection sort " + Arrays.toString(arr));
+        return arr;
     }
 
-    public int[] convertToAscii(char[] array){
+    public int[] convertToAscii(char[] array) {
         int[] result = new int[array.length];
 
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             int ascii = (int) array[i];
             result[i] = ascii;
         }
-
+        System.out.println("convert to ascii" + Arrays.toString(result));
         return result;
     }
 
-    public char[] convertToChar(int[] array){
+    public char[] convertToChar(int[] array) {
         char[] result = new char[array.length];
 
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             char character = (char) array[i];
             result[i] = character;
         }
-
+        System.out.println("convert to char " + Arrays.toString(result));
         return result;
     }
 
-    public static char[] sortCharacters(char[] array){
+    public static char[] sortCharacters(char[] array) {
         AlphabetSort foo = new AlphabetSort();
         int[] intTransfer = foo.convertToAscii(array);
         intTransfer = foo.selectionSort(intTransfer);
@@ -54,9 +56,9 @@ public class AlphabetSort {
 
 
     public static void main(String[] args) {
-        char[] test = {'b','y','e','a','u','r'};
+        char[] test = {'b', 'y', 'e', 'a', 'u', 'r', 'l', 'i', 'o'};
         char[] newArray = sortCharacters(test);
         System.out.println(Arrays.toString(newArray));
-        
+
     }
 }
